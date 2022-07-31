dee2167e5b195fc54d06a4cacde22bd0bd222655
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-07-31 16:34:35 +0500

    home done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { Menu, X } from "react-feather";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import logoLight from "../assets/logoLight.png";
 import { logout } from "../services/auth";
+import OutsideClickHandler from "react-outside-click-handler";
 
 export function Header({
   dataWallet,
@@ -28,98 +30,129 @@ export function Header({
   }, []);
 
   return (
-    <div className="header">
-      <Link to="/" className="header__logo">
-        <img src={logo} alt="logo" className="header__logo__img" />
-      </Link>
-      <button
-        className="header__nav__menu"
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-      >
-        {isOpen ? (
-          <X size={20} color="#fff" />
-        ) : (
-          <Menu size={20} color="#fff" />
-        )}
-      </button>
-      {isOpen ? (
-        <div className="header__nav">
-          <Link to="/stake" className="header__nav__link">
-            Stake
-          </Link>
-          <Link to="/" className="header__nav__link">
-            Rewards
-          </Link>
-          <a href="#" className="header__nav__link">
-            About Us
-          </a>
-          {user === null ? (
-            <>
-              <a
-                href="#"
-                onClick={() => {
-                  setIsLogin(true);
-                }}
-                className="header__nav__button"
-              >
-                Login
-              </a>
-            </>
+    <div
+      className="header__wrapper"
+      style={isOpen && window.innerWidth < 768 ? { background: "white" } : null}
+    >
+      <div className="header">
+        <Link to="/" className="header__logo">
+          <img
+            src={isOpen && window.innerWidth < 768 ? logo : logoLight}
+            alt="logo"
+            className="header__logo__img"
+          />
+        </Link>
+        <button
+          className={
+            isOpen
+              ? "header__nav__menu header__nav__menu__active"
+              : "header__nav__menu"
+          }
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          {isOpen ? (
+            <X size={20} color="currentColor" />
           ) : (
-            <>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  fontSize: 14,
-                }}
-              >
-                <img
-                  src={user.photoURL}
-                  alt={user.displayName}
-                  style={{
-                    width: 50,
-                    height: 50,
-                    borderRadius: 50,
-                  }}
-                />
-                {/* {user.displayName} */}
-              </div>
-              {dataWallet.Balance === null ? (
-                <a
-                  href="#"
-                  onClick={connectWallet}
-                  className="header__nav__button"
-                >
-                  Connect Wallet
-                </a>
-              ) : (
-                <a
-                  href="#"
-                  className="header__nav__button"
-                  style={{ background: "black" }}
-                >
-                  Connected Wallet
-                </a>
-              )}
-
-              <a
-                href="#"
-                onClick={() => {
-                  logout(setUser);
-                  setUser(null);
-                }}
-                className="header__nav__button"
-                style={{ background: "black" }}
-              >
-                Logout
-              </a>
-            </>
+            <Menu size={20} color="currentColor" />
           )}
-        </div>
-      ) : null}
+        </button>
+        {isOpen ? (
+          <div className="header__nav">
+            <OutsideClickHandler
+              display="flex"
+              onOutsideClick={() => {
+                if (window.innerWidth < 768) {
+                  setIsOpen(false);
+                }
+              }}
+            >
+              <div className="header__nav " style={{ top: 0 }}>
+                <Link to="/stake" className="header__nav__link">
+                  Stake
+                </Link>
+                <Link to="/" className="header__nav__link">
+                  Rewards
+                </Link>
+                <a href="#" className="header__nav__link">
+                  About Us
+                </a>
+                {user === null ? (
+                  <>
+                    <a
+                      href="#"
+                      onClick={() => {
+                        setIsLogin(true);
+                        if (window.innerWidth < 768) {
+                          setIsOpen(false);
+                        }
+                      }}
+                      className="header__nav__button"
+                    >
+                      Login
+                    </a>
+                  </>
+                ) : (
+                  <>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        fontSize: 14,
+                      }}
+                    >
+                      <img
+                        src={user.photoURL}
+                        alt={user.displayName}
+                        style={{
+                          width: 50,
+                          height: 50,
+                          borderRadius: 50,
+                        }}
+                      />
+                      {/* {user.displayName} */}
+                    </div>
+                    {dataWallet.Balance === null ? (
+                      <a
+                        href="#"
+                        onClick={connectWallet}
+                        className="header__nav__button"
+                      >
+                        Connect Wallet
+                      </a>
+                    ) : (
+                      <a
+                        href="#"
+                        className="header__nav__button"
+                        style={{ background: "black" }}
+                      >
+                        Connected Wallet
+                      </a>
+                    )}
+
+                    <a
+                      href="#"
+                      onClick={() => {
+                        logout(setUser);
+                        setUser(null);
+                      }}
+                      className="header__nav__button"
+                      style={{
+                        background: "black",
+                        color: "white",
+                        borderColor: "white",
+                      }}
+                    >
+                      Logout
+                    </a>
+                  </>
+                )}
+              </div>
+            </OutsideClickHandler>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import banner from "../assets/banner.png";
-import icon from "../assets/icon.svg";
+import car from "../assets/car.png";
 import { StatsBoardFilterEntry } from "../components/StatsBoardFilterEntry";
 import { StatsBoardTableList } from "../components/StatsBoardTableList";
 
@@ -8,27 +7,30 @@ export default function Home({ setIsStartGame, user, setIsLogin }) {
   return (
     <>
       <div className="home__banner">
-        <img src={banner} alt="banner" className="home__banner__img" />
-        <div className="home__banner__overlay">
-          <img src={icon} alt="icon" className="home__banner__overlay__img" />
-          <div className="home__banner__overlay__content">
-            <div className="home__banner__overlay__content__heading">
-              TypEarn
-            </div>
-            <div className="home__banner__overlay__content__info">
+        <div className="home__banner__content">
+          <div className="home__banner__content__left">
+            <div className="home__banner__content__left__heading">TypEarn</div>
+            <div className="home__banner__content__left__info">
               In publishing and graphic design, Lorem ipsum is a placeholder
               text commonly used to demonstrate the visual form of a document or
               a typeface without relying on meaningful content. Lorem ipsum may
               be used as a placeholder before final copy is available. Wikipedia
             </div>
             <button
-              className="home__banner__overlay__content__button"
+              className="home__banner__content__left__button"
               onClick={() => {
                 user === null ? setIsLogin(true) : setIsStartGame(true);
               }}
             >
               Start the game
             </button>
+          </div>
+          <div className="home__banner__content__right">
+            <img
+              src={car}
+              alt="car"
+              className="home__banner__content__right__img"
+            />
           </div>
         </div>
       </div>
