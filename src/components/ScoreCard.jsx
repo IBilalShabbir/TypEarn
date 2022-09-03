846af27fb288ebf459ba20e756a35ec723567fa3
Bilal Shabbir <102783392+IBilalShabbir@users.noreply.github.com>
2022-09-03 17:10:33 +0500

    Pop up fixes done

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import scorecardtop from "../assets/scorecardtop.png";
+import { useEffect } from "react";
 
 export default function ScoreCard({ onClose, FirstPosition }) {
+  const [isChecked, setIschecked] = React.useState(false);
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
   return (
-    <div className="scorecard__reverse">
-      <form className="scorecard__reverse__form">
+    <div className="popup__reverse">
+      <form className="popup__reverse__form">
         <div className="scorecard__reverse__form__img">
           <img src={scorecardtop} alt="" />
         </div>
